4a73c513dc59cb0e2cf2d84f15412f1eab2ffdb3
Raul Nohea Goodness <raulg@hekili.net>
2021-12-21 18:28:36 -1000

    add yup validation to the form

--- src/routes/form.svelte
+++ src/routes/form.svelte
@@ -1,5 +1,25 @@
 <script>
 	import { createForm } from 'svelte-forms-lib';
+	import * as yup from 'yup';
+
+    const validator = yup.object().shape({
+        fullname: yup.string().required(),
+        prefix: yup.string(),
+        profile: yup.object().shape({
+            address: yup
+                .string()
+                .required(),
+            gender: yup
+                .string()
+        }),
+        contacts: yup.array().of(
+            yup.object().shape({
+                contacttype: yup.string(),
+                name: yup.string().required(),
+                email: yup.string(),
+            })
+        )
+    });
 
 	const formProps = {
 		initialValues: {
@@ -11,6 +31,7 @@
 			},
 			contacts: []
 		},
+        validationSchema: validator,
 		onSubmit: (values) => {
 			console.log('onSubmit (via handleSubmit): ', JSON.stringify(values));
 		}
@@ -34,7 +55,7 @@
 <div>
 	<h1>Complex Svelte Form Example</h1>
 
-	<h4>Test Form</h4>
+	<h3>Test Form</h3>
 	<form on:submit={handleSubmit}>
 		<div>
 			<label for="fullname"> Full Name </label>
@@ -68,7 +89,53 @@
 			{/if}
 		</div>
 
+        <h4>Contacts</h4>
+        {#each $form.contacts as contact, j}
+          <div class="form-group">
+            <div>
+              <label for={`contacts[${j}].name`}>Name</label>
+              <input
+                name={`contacts[${j}].name`}
+                placeholder="name"
+                on:change={handleChange}
+                on:blur={handleChange}
+                bind:value={$form.contacts[j].name}
+              />
+              
+              {#if $errors?.contacts[j]?.name}
+                <div class="error-text">{$errors.contacts[j].name}</div>
+              {/if}
+            </div>
+    
+            <div>
+                <label for={`contacts[${j}].email`}>Email</label>
+                <input
+                placeholder="email"
+                name={`contacts[${j}].email`}
+                on:change={handleChange}
+                on:blur={handleChange}
+                bind:value={$form.contacts[j].email}
+              />
+              {#if $errors?.contacts[j]?.email}
+                <div class="error-text">{$errors.contacts[j].email}</div>
+              {/if}
+            </div>
+    
+            {#if $form.contacts.length === j + 1}
+                <button type="button" on:click={removecontact(j)}>[- remove last contact]</button>
+            {/if}
+          </div>
+        {/each}
+    
+        {#if $form.contacts}
+            <div>
+                <button on:click|preventDefault={addcontact}>[+ add contact]</button>
+            </div>
+        {/if}
+
+        <div>
 		<input type="submit" name="submit" value="submit button" />
+        </div>
 	</form>
 </div>
 
